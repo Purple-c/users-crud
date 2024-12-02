0b9717a1cfe10b2b2bf4d981dfe2e021e3815ba5
Carlos S <glor.exe@gmail.com>
2024-12-01 22:32:18 -0400

    commit

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+/*
+const myDiv = document.getElementById('my-div')
+
+
+
+const myList = document.getElementById('my-list')
+const createItemButton = document.getElementById('create-item')
+
+createItemButton.addEventListener('click', () => {
+    const item = document.createElement('li') //Creamos elemento id
+    const text = document.createElement('p')
+    text.classList.add('item-text')
+    text.textContent = 'Estoy vivo!!!!' //Agregamos text content pero no esta anclado a nada
+
+    item.appendChild(text)
+    myList.appendChild(item)
+})
+*/
+
+
